b1ed53c01882ff4f1e6849e07473c054114baa0e
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-06-12 23:30:56 +0400

    src/parce.c norm fix

--- src/parse.c
+++ src/parse.c
@@ -1,31 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 23:22:22 by root              #+#    #+#             */
+/*   Updated: 2025/06/12 23:30:19 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../solong.h"
 
-void	parse_map(t_data *data, char *filename)
+static char	*read_map_file(char *filename)
 {
 	int		fd;
 	char	*line;
 	char	*full;
 	char	*tmp;
 
-	if (ft_strlen(filename) < 5 || ft_strncmp(filename + ft_strlen(filename) - 4, ".ber", 4))
-		ft_exit_error("Invalid file extension");
-	fd = open(filename, 0, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		ft_exit_error("Failed to open map file");
 	full = ft_strdup("");
-	while ((line = get_next_line(fd)) != NULL)
+	if (!full)
+		ft_exit_error("Malloc failed");
+	line = get_next_line(fd);
+	while (line)
 	{
 		tmp = full;
 		full = ft_strjoin(full, line);
 		free(tmp);
 		free(line);
+		line = get_next_line(fd);
 	}
 	close(fd);
+	return (full);
+}
+
+void	parse_map(t_data *data, char *filename)
+{
+	char	*full;
+
+	if (ft_strlen(filename) < 5
+		|| ft_strncmp(filename + ft_strlen(filename) - 4, ".ber", 4))
+		ft_exit_error("Invalid file extension");
+	full = read_map_file(filename);
 	if (!full || !*full)
 		ft_exit_error("Empty map file");
 	data->map.map = ft_split(full, '\n');
 	free(full);
-
 	data->map.y = 0;
 	while (data->map.map[data->map.y])
 		data->map.y++;
@@ -33,6 +57,13 @@ void	parse_map(t_data *data, char *filename)
 	data->collectibles = 0;
 }
 
+/**
+* @file parse.c
+*
+* @brief This function searches through the map to find the player's position
+* @param data Pointer to the game data structure
+* @returns void
+*/
 void	find_player_position(t_data *data)
 {
 	int	x;
